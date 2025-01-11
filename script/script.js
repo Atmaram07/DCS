608fd5956679d48146e83e791490d5e2a8ff785b
Atmaram Patra <ag4atmaramgreat@gmail.com>
2025-01-11 22:51:50 +0530

    newletter deactivate

--- script/script.js
+++ script/script.js
@@ -9,13 +9,6 @@ function sendMail() {
   emailjs.send("service_1nfiahv", "template_uhg5itp", parms).then(alert("Message sent successfully"));
 }
 
-// Show the newsletter modal when the page finishes loading
-window.addEventListener("load", function () {
-  var newsletterModal = new bootstrap.Modal(
-    document.getElementById("newsletterModal")
-  );
-  newsletterModal.show();
-});
 
 // Toggle the arrow icon on collapse toggle
 const dropdownButtons = document.querySelectorAll(
